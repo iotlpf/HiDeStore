715ab608e93bfcb8e588323a65c96b16ea4aead9
HiDeStore <iotlpf@163.com>
2020-09-30 15:48:03 +0800

    add file

---
+++ script/drestor.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+dataset="kernel"
+
+rbv=10
+
+if [ $# -gt 0 ];then
+    rbv=$1
+fi
+
+kernel_path="/home/lpf/workspace/kernel_data_debug/"
+#kernel_path="/home/lpf/workspace/kernel/kernel"
+gcc_path="/home/lpf/workspace/gcc/gcc"
+fsl_path="/home/lpf/workspace/fslhomes/fsl/"
+macos_path="/home/lpf/workspace/macos/macos"
+
+
+case $dataset in
+	"kernel")
+		path=$kernel_path
+		;;
+	"gcc")
+		path=$gcc_path
+		;;
+	"fsl")
+		path=$fsl_path
+		;;
+	"macos")
+		path=$macos_path
+		;;
+	*)
+		echo "Wrong dataset!"
+		exit 1
+		;;
+esac
+
+
+DIR=/home/lpf/workspace/restore/
+rm -rf ${DIR}/*
+
+> drlog
+for ((i=0;i<$rbv;i++))
+do
+	echo "restore job ${i}"
+	mkdir -p ${DIR}/destor
+	./build/destor -r${i} ${DIR}/destor >> drlog
+	echo " " >> drlog
+done
